78cfaa6dde51725b7fe0fc4f3584f4560a4472c8
AKA <aka@aviareal.com>
2019-12-20 21:22:29 +0300

    Исследую класс AbstractMap

---
+++ Assets/Scenes/Scripts/sCommonParameters.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mapbox.Unity.Map;
+
+public class sCommonParameters : MonoBehaviour
+{
+    public Vector3 GlobalScale = Vector3.one;
+    [SerializeField] private AbstractMap _myAbsMap;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("1"))
+        {
+            print("InitializeOnStart = " + _myAbsMap.InitializeOnStart);
+            print("InitialZoom = " + _myAbsMap.InitialZoom);
+        }
+    }
+}
